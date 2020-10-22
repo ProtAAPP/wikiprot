a4e5747280d4b167802ca66d1ef84b921c8af57a
Jose Angel Alvarez Perez <JAP038@aytomadrid.munimadrid.es>
2020-10-23 00:11:49 +0200

    rename folders

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,27 +34,14 @@ module.exports =
                { text: 'Introducción', link: '/01_introduccion/' },
                { text: 'Normativa y Organismos', link: '/02_normativa/' },
                { text: 'Gestión de la seguridad', link: '/03_gestion/' },
-/*
-               { text: 'Logs', link: '/04_logs/' },
-               { text: 'Criptografía', link: '/05_criptografia/' },
-               { text: 'Malware', link: '/06_malware/' },
-               { text: 'Seguridad perimetral', link: '/07_seguridad_perimetral/' },
-               { text: 'Redes', link: '/08_redes/' },
-               { text: 'Bastionado', link: '/09_bastionado/' },
-               { text: 'Web', link: '/10_web/' },
-               { text: 'OSINT', link: '/11_osint/' },
-               { text: 'Reputación', link: '/12_reputacion/' },
-               { text: 'Internet of Things', link: '/13_iot/' },
-               { text: 'Auditorías', link: '/14_auditorias/' }
-*/
-               { text: 'Protección: Arquitecturas y Tecnologías', link: '/new_04_tecnologias/' },
-               { text: 'Seguridad en el Desarrollo', link: '/new_05_desarrollo/' },
-               { text: 'Gestión de Vulnerabilidades', link: '/new_11_vulnerabilities/' },
-               { text: 'Detección y Respuesta de Ciberincidentes', link: '/new_06_operaciones/' },
-               { text: 'Amenazas, Ataques y Vulnerabilidades', link: '/new_08_amenazas/' },
-               { text: 'Auditorías de Vulnerabilidades y Hacking Ético', link: '/new_09_hacking/' },
-               { text: 'Formación, Concienciación y Difusión', link: '/new_07_formacion/' },               
-               { text: 'Eventos de Ciberseguridad', link: '/new_10_eventos/' }               
+               { text: 'Protección: Arquitecturas y Tecnologías', link: '/04_tecnologias/' },
+               { text: 'Seguridad en el Desarrollo', link: '/05_desarrollo/' },
+               { text: 'Gestión de Vulnerabilidades', link: '/11_vulnerabilities/' },
+               { text: 'Detección y Respuesta de Ciberincidentes', link: '/06_operaciones/' },
+               { text: 'Amenazas, Ataques y Vulnerabilidades', link: '/08_amenazas/' },
+               { text: 'Auditorías de Vulnerabilidades y Hacking Ético', link: '/09_hacking/' },
+               { text: 'Formación, Concienciación y Difusión', link: '/07_formacion/' },               
+               { text: 'Eventos de Ciberseguridad', link: '/10_eventos/' }               
             ]
          }
       ],
@@ -69,14 +56,14 @@ module.exports =
          '01_introduccion/',
          '02_normativa/',
          '03_gestion/',
-         'new_04_tecnologias/',
-         'new_05_desarrollo/',
-         'new_11_vulnerabilities/',
-         'new_06_operaciones/',
-         'new_08_amenazas/',
-         'new_09_hacking/',
-         'new_07_formacion/',         
-         'new_10_eventos/'
+         '04_tecnologias/',
+         '05_desarrollo/',
+         '11_vulnerabilities/',
+         '06_operaciones/',
+         '08_amenazas/',
+         '09_hacking/',
+         '07_formacion/',         
+         '10_eventos/'
       ]
    }
    
